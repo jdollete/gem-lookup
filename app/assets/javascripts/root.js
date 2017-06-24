373fc180ad61abec44ff5c91c9dae6525527c69e
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-06-24 13:03:51 -0400

    Set variables for api request data

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -14,10 +14,17 @@ $( document ).ready(function() {
       url: "https://rubygems.org/api/v1/gems/" + gemSearch + ".json",
 
       success: function (data) {
-        console.log(data.name);
-        console.log(data.info);
-        console.log(data.gem_uri);
-        console.log(data.dependencies.development[0].name); // Make sure to deal with edge case of it being empty
+        var gemName = data.name;
+        var gemDescription = data.info;
+        var gemLink = data.gem_uri;
+        var gemDepencies = [];
+
+        if (data.dependencies.development !== 0) {
+          data.dependencies.development.forEach(function(element) {
+            gemDepencies.push(element.name);
+          });
+        }
+
       },
       error: function (error) {
       }
