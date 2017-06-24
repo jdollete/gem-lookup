8320488e7368b70b7df1539126ff1bca532b6ec1
jdollete <[01;31m[K"email"[m[K: "allain.dollete@gmail.com>
2017-06-24 12:37:11 -0400

    Setup Ajax fail function

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -11,15 +11,19 @@ $( document ).ready(function() {
       type: "GET",
       dataType: "json",
       url: "https://rubygems.org/api/v1/gems/" + gemSearch + ".json",
+
       success: function (data) {
+        debugger;
         console.log(data.name);
         console.log(data.info);
         console.log(data.gem_uri);
-        console.log(data.dependencies.development[0].name);
+        console.log(data.dependencies.development[0].name); // Make sure to deal with edge case of it being empty
       },
       error: function (error) {
-
       }
+    })
+    .fail(function() {
+      alert("Did not find gem");
     });
   });
 });
